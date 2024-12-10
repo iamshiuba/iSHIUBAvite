d95ae4512a04d64e8d52f25db158e5a38f4893da
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-12-10 12:27:13 -0300

    List component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button from "./Button.jsx";
 import Student from "./Students.jsx";
+import UserGreeting from "./UserGreeting.jsx";
+import List from "./List.jsx";
 
 function App() {
+  const fruits = [{id: 1, name:"apple", calories: 95}, 
+                  {id: 2, name: "orange", calories: 45}, 
+                  {id: 3, name: "banana", calories: 105}, 
+                  {id: 4, name: "coconut", calories: 159}, 
+                  {id: 5, name: "pineapple", calories: 37}];
+
+  const vegetables = [{id: 1, name:"potatoes", calories: 110}, 
+                      {id: 2, name: "celery", calories: 15}, 
+                      {id: 3, name: "carrots", calories: 25}, 
+                      {id: 4, name: "corn", calories: 63}, 
+                      {id: 5, name: "broccoli", calories: 50}];
+
   return (
     <>
       <Header />
@@ -13,11 +27,14 @@ function App() {
       <Card />
       <Card />
       <Food />
+      {fruits.length > 0 && <List items={fruits} category="Fruits"/>}
+      {vegetables.length > 0 && <List items={vegetables} category="Vegetables"/>}
       <Student name="Spongebob" age={30} isStudent={true} />
       <Student name="Patrick" age={42} isStudent={false} />
       <Student name="Squiddward" age={50} isStudent={false} />
       <Student name="Sandy" age={27} isStudent={true} />
       <Student />
+      <UserGreeting isLoggedIn={true} username="IamSHIUBA" />
       <Footer />
     </>
   );
